749f8aef7a3318070cdcd5632137a490bf345f6d
SwagMC <31862231+SwagMC@users.noreply.github.com>
2018-01-18 00:10:04 -0500

    add search nearby request

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs, Button, Spin } from 'antd';
-import { GEO_OPTIONS } from '../constants';
+import {API_ROOT, GEO_OPTIONS, POS_KEY, AUTH_PREFIX, TOKEN_KEY} from '../constants';
+import $ from 'jquery';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
@@ -29,6 +30,9 @@ export class Home extends React.Component {
     onSuccessLoadGeoLocation = (position) => {
         console.log(position);
         this.setState({ loadingGeoLocation: false, error: ''});
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem(POS_KEY, JSON.stringify({lat: latitude, lon: longitude}));
+        this.loadNearbyPosts();
     }
     onFailedLoadGeoLocation = () => {
         this.setState({ loadingGeoLocation: false, error: 'Loading geo location failed!' });
@@ -42,6 +46,25 @@ export class Home extends React.Component {
         } else {
             return null;
         }
+    }
+
+    loadNearbyPosts = () => {
+        //const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+        const lat = 37.7915953;
+        const lon = -122.3937977;
+        $.ajax({
+            url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
+            method: 'GET',
+            headers: {
+                Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
+            },
+        }).then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.log(error);
+        }).catch((error) => {
+            console.log(error);
+        });
     }
 
     render() {
